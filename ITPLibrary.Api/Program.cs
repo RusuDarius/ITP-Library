d8b0ea6d90b2ea89e7b79e44cb2adcfdf11baa83
Rusu Darius <dariusrusu7@gmail.com>
2024-08-04 16:53:48 +0300

    Solved connection issue with db

--- ITPLibrary.Api/Program.cs
+++ ITPLibrary.Api/Program.cs
@@ -23,9 +23,21 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration");
+}
 
 app.Run();
