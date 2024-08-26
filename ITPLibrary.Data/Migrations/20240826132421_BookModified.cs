73215e5b86d1f2e4d87a05e358de369df6901e79
Rusu Darius <dariusrusu7@gmail.com>
2024-08-26 16:25:35 +0300

    BookModified with promoted migration

---
+++ ITPLibrary.Data/Migrations/20240826132421_BookModified.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITPLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "AddedDateTime",
+                table: "Books",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPromoted",
+                table: "Books",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Thumbnail",
+                table: "Books",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddedDateTime",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "IsPromoted",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Thumbnail",
+                table: "Books");
+        }
+    }
+}
